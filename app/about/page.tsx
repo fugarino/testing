4f168cfa1f5e58e77c209a515e70d7e4f9426ad2
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-03-11 14:46:56 -0500

    test

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      about page
+      <Link href="/">home</Link>
+    </div>
+  );
+};
+
+export default page;
